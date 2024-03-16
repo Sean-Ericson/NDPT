8457583dc8334f204d532ac30f3ca9ceb90a82cc
Sean-Ericson <sericson314@gmail.com>
2024-03-16 11:46:51 -0700

    ah

--- NDPT_energy.py
+++ NDPT_energy.py
@@ -1,13 +1,13 @@
-from decimal import InvalidOperation
 import numpy as np
-import math
+from collections import defaultdict
+import pickle
 
 # rotate list ls to the right by n (negative to go left)
 def rotate_list(ls, n):
     return ls[-n:] + ls[:-n]
 
 # Determine if two lists are rotated versions of each other
-def cyclic_list_equal(l1, l2):
+def list_cyclic_equal(l1, l2):
     # Normal list equal
     def list_equal(l1, l2):
         for i in range(len(l1)):
@@ -23,30 +23,24 @@ def cyclic_list_equal(l1, l2):
             return True
     return False
 
-def set_lists_equal(l1, l2):
-    s1 = set(l1)
-    s2 = set(l2)
-    return s1.issubset(s2) and s2.issubset(s1)
+# Determine if two lists are equal as sets. (can't use set() if the elements aren't hashable)
+def list_set_equal(l1, l2):
+    return np.all(np.array([x in l2 for x in l1])) and np.all(np.array(x in l1 for x in l2))
 
-def insert(i, x, ls):
-    l = list(ls)
-    l.insert(i,x)
-    return l
-
-# returns all *lists* n non-negative numbers that sum to m
-def partition(n,m):
-    res = []
-    if m == 0:
-        return [[0 for _ in range(n)]]
-    if n == 1:
-        return [[m]]
-    for i in range(m+1):
-        for ls in partition(n-1, m-i):
-            res += [[i] + ls]
-    return res
-
-# Return all first/last-term-canceling partitions for correction n
+# Return all non first/last-term-canceling partitions for correction n
 def smart_partition(n):
+    # returns all *lists* n non-negative numbers that sum to m
+    def partition(n,m):
+        res = []
+        if m == 0:
+            return [[0 for _ in range(n)]]
+        if n == 1:
+            return [[m]]
+        for i in range(m+1):
+            for ls in partition(n-1, m-i):
+                res += [[i] + ls]
+        return res
+
     res = []
     # zero on each side
     for ls in partition(n-1, n-1):
@@ -58,42 +52,40 @@ def smart_partition(n):
                 res.append([i] + ls + [j])
     return res
 
-def combine_first_last(ls):
-    if (ls[0] == 0 and ls[-1] != 0) or (ls[-1] == 0 and ls[0] != 0):
-        raise InvalidOperation()
-    if ls[0] == 0:
-        return DeltaTerm(-1, ls[:-1])
-    return DeltaTerm(1, [ls[0] + ls[-1]] + ls[1:-1])
-
-class DeltaTerm:
-    def __init__(self, coeff, ls):
-        self.coeff = coeff
-        self.ls = ls
-
-    def __eq__(self, other) -> bool:
-        return cyclic_list_equal(other.ls, self.ls) and other.coeff == self.coeff
-
-    def __add__(self, other):
-        if not cyclic_list_equal(other.ls, self.ls):
-            raise InvalidOperation()
-        return DeltaTerm(self.coeff + other.coeff)
-
-    def __str__(self) -> str:
-        return "{:d} * {}".format(self.coeff, self.ls)
-
-    def __repr__(self) -> str:
-        return str(self)
-
-def resolve_signs(dterm):
-    return DeltaTerm(dterm.coeff * ((-1)**sum([1 for i in range(len(dterm.ls)) if dterm.ls[i] == 0])), dterm.ls)
-
-
+class PartitionGenerator:
+    partitions = dict()
+    
+    def partition(self, n, m):
+        if (n,m) in PartitionGenerator.partitions.keys():
+            return PartitionGenerator.partitions[(n,m)]
+        res = []
+        if m == 0:
+            return [[0 for _ in range(n)]]
+        if n == 1:
+            return [[m]]
+        for i in range(m+1):
+            for ls in self.partition(n-1, m-i):
+                res += [[i] + ls]
+        PartitionGenerator.partitions[(n,m)] = res
+        return res
+    
+    def noncanceling_partitions(self, n):
+        res = []
+        # zero on each side
+        for ls in self.partition(n-1, n-1):
+            yield [0] + ls + [0]
+        # nonzero on each side
+        for i in range(1, n):
+            for j in range(1, n-i):
+                for ls in self.partition(n-1, n-1-i-j):
+                    yield [i] + ls + [j]
+        return res
 
 class MultiSet:
     def __init__(self) -> None:
         self.items = []
 
-    def add(self, new_item, count):
+    def add(self, new_item, count, auto_clear=True):
         need_to_append = True
         for i in range(len(self.items)):
             current_item, current_count = self.items[i]
@@ -103,24 +95,19 @@ class MultiSet:
                 break
         if need_to_append:
             self.items.append(tuple((new_item, count)))
+        if auto_clear:
+            self.clear_zero_count_items()
+
+    def add_many(self, items, counts):
+        for item, count in zip(items, counts):
+            self.add(item, count, auto_clear=False)
         self.clear_zero_count_items()
 
     def clear_zero_count_items(self):
         self.items = [i for i in self.items if i[1] != 0]
 
     def __eq__(self, other):
-        if len(self.items) != len(other.items):
-            return False
-        for item in self.items:
-            val, count = item
-            match_found = False
-            for i in other.items:
-                if i[0] == val and i[1] == count:
-                    match_found = True
-                    break
-            if not match_found:
-                return False
-        return True
+        return list_set_equal(self.items, other.items)
 
     def __str__(self):
         return " ".join(["{}^{:d}".format(i[0], i[1]) if i[1] != 1 else "{}".format(i[0]) for i in self.items])
@@ -129,112 +116,117 @@ class MultiSet:
         return str(self)
 
 class SigmaTerm:
-    def __init__(self,exps):
-        self.exps = exps
+    def __init__(self, indices):
+        self.indices = indices
 
     def __eq__(self, other: object) -> bool:
-        return len(self.exps) == len(other.exps) and np.all(np.array(self.exps) == np.array(other.exps))
+        return len(self.indices) == len(other.indices) and np.all(np.array(self.indices) == np.array(other.indices))
     
     def __str__(self):
-        return "Sigma({})".format(self.exps)
+        return "Sigma({})".format(self.indices)
 
     def __repr__(self) -> str:
         return str(self)
 
     def ToLatex(self):
-        return "\\Sigma_{{}}".format(self.exps)
+        return "\\Sigma_{{}}".format(self.indices)
 
 class PerturbativeTerm:
-    def __init__(self, v_exp, sigmas, coeff) -> None:
+    def __init__(self, v_exp, sigmas) -> None:
         self.v_exp = v_exp
         self.sigmas = sigmas
-        self.coeff = coeff
+
+    @staticmethod
+    def FromPartition(partition):
+        # Combine first/last
+        partition = [partition[0] + partition[-1]] + partition[1:-1]
+
+        #rotate to start at a boundary 0
+        for i in range(len(partition)):
+            if partition[i] != 0 and partition[(i+1)%len(partition)] == 0:
+                shift = i+1    
+        partition = rotate_list(partition, -shift)
+        
+        pterm = PerturbativeTerm(0, MultiSet())
+        
+        # get info for v_exp
+        zero_run_lens = [1]
+        on_run = True
+        for i in range(1, len(partition)):
+            if partition[i] == 0:
+                if on_run:
+                    zero_run_lens[-1] += 1
+                else:
+                    zero_run_lens.append(1)
+                    on_run = True
+            else:
+                on_run = False
+        pterm.v_exp = sum(zero_run_lens) - len(zero_run_lens)
+
+        # get sigmas
+        number_groups = []
+        in_group = False
+        for i in range(1, len(partition)):
+            if partition[i] != 0:
+                if in_group:
+                    number_groups[-1].append(partition[i])
+                else:
+                    number_groups.append([partition[i]])
+                    in_group = True
+            else:
+                in_group = False
+        pterm.sigmas.add_many(*zip(*[(SigmaTerm(group), 1) for group in number_groups]))
+        
+        return pterm
 
     def __eq__(self, other):
-        return self.v_exp == other.v_exp and self.coeff == other.coeff and self.sigmas == other.sigmas
+        return self.v_exp == other.v_exp and self.sigmas == other.sigmas
 
     def __str__(self) -> str:
-        return "{:d} * (V_00 ^ {:d}) ".format(self.coeff, self.v_exp) + str(self.sigmas) if self.v_exp > 0 else "{:d} * ".format(self.coeff) + str(self.sigmas)
+        return "(V_00 ^ {:d}) ".format(self.v_exp) + str(self.sigmas) if self.v_exp > 0 else ""
     
+    def string_with_coeff(self, coeff):
+        return "{:d} * (V_00 ^ {:d}) ".format(coeff, self.v_exp) + str(self.sigmas) if self.v_exp > 0 else "{:d} * ".format(coeff) + str(self.sigmas)
+
     def __repr__(self) -> str:
         return str(self)
 
-def DeltaToPerturb(dterm):
-    if dterm.ls[0] != 0:
-        #rotate to start at a 0
-        i = dterm.ls.index(0)
-        dterm.ls = dterm.ls[i:] + dterm.ls[:i]
+class EnergyCorrection:
+    def __init__(self, n):
+        self.n = n
+        self.SigmasByV00 = dict()
+
+    def calc(self):
+        # Generate perms and combine first/last 
+        p_terms = MultiSet()
+        partGen = PartitionGenerator()
+        for part in partGen.noncanceling_partitions(self.n):
+            p_terms.add(PerturbativeTerm.FromPartition(part), (-1)**(part.count(0)), auto_clear=False)
+        p_terms.clear_zero_count_items()
+
+        #Sort by V00
+        self.v_max = max(list(zip(*p_terms.items))[0], key=lambda t: t.v_exp).v_exp
+        for i in range(self.v_max + 1):
+            self.SigmasByV00[i] = [t for t in p_terms.items if t[0].v_exp == i]
+
+        self.p_terms = p_terms
+
+    def print_pterms_by_v00(self):
+        for i in range(self.v_max + 1):
+            for t in [t for t in self.p_terms.items if t[0].v_exp == i]:
+                print(t[0].string_with_coeff(t[1]))
+        
+    def to_list(self):
+        return [(v_exp, [([(sigma.indices, exp) for sigma,exp in sigmas.sigmas.items], coeff) for sigmas,coeff in pterm]) for v_exp, pterm in self.SigmasByV00.items()]
     
-    pterm = PerturbativeTerm(0, MultiSet(), dterm.coeff)
-    
-    # get info for v_exp
-    zero_run_lens = [1]
-    on_run = True
-    for i in range(1, len(dterm.ls)):
-        if dterm.ls[i] == 0:
-            if on_run:
-                zero_run_lens[-1] += 1
-            else:
-                zero_run_lens.append(1)
-                on_run = True
-        else:
-            on_run = False
-    pterm.v_exp = sum(zero_run_lens) - len(zero_run_lens)
+correction = EnergyCorrection(4)
+correction.calc()
+correction.print_pterms_by_v00()
 
-    # get sigmas
-    number_groups = []
-    in_group = False
-    for i in range(1, len(dterm.ls)):
-        if dterm.ls[i] != 0:
-            if in_group:
-                number_groups[-1].append(dterm.ls[i])
-            else:
-                number_groups.append([dterm.ls[i]])
-                in_group = True
-        else:
-            in_group = False
-    for group in number_groups:
-        pterm.sigmas.add(SigmaTerm(group), 1)
-    
-    return pterm
-
-def print_pterms_by_v00(pterms):
-    v_max = max(pterms, key=lambda t: t.v_exp).v_exp
-    for i in range(v_max + 1):
-        for t in [t for t in pterms if t.v_exp == i]:
-            print(t)
-    
-n = 4
-possible_terms = smart_partition(n)
-combined_terms = [combine_first_last(ls) for ls in possible_terms] # combine the first and last indicies of each term, creating list of DeltaTerm objects
-resolved_terms = [resolve_signs(ls) for ls in combined_terms] # resolve the signs of the DeltaTerm objects (odd # of 0s add a - sign)
-
-# Consolidate the terms together (add like terms)
-consolidated_terms = [resolved_terms[0]]
-for term in resolved_terms[1:]:
-    need_to_append = True
-    for inserted_term in consolidated_terms:
-        if cyclic_list_equal(term.ls, inserted_term.ls):
-            inserted_term.coeff += term.coeff
-            need_to_append = False
-            break
-    if need_to_append:
-        consolidated_terms.append(term)
-
-consolidated_terms = [t for t in consolidated_terms if t.coeff != 0] # remove terms w/ coeff 0
-pterms = [DeltaToPerturb(term) for term in consolidated_terms] # convert from DeltaTerms to PerturbativeTerms
-
-# Consolidate pterms together (add like terms)
-consolidated_pterms = [pterms[0]]
-for term in pterms[1:]:
-    need_to_append = True
-    for inserted_term in consolidated_pterms:
-        if term.sigmas == inserted_term.sigmas:
-            inserted_term.coeff += term.coeff
-            need_to_append = False
-            break
-    if need_to_append:
-        consolidated_pterms.append(term)
-
-consolidated_pterms = [t for t in consolidated_pterms if t.coeff != 0] # remove terms w/ coeff 0
-print_pterms_by_v00(consolidated_pterms)
+"""for i in range(2,13):
+    correction = EnergyCorrection(i)
+    correction.calc()
+    data = correction.to_list()
+    print("Terms in correction {}: {}".format(i, len(correction.p_terms.items)))
+    #with open("Coded_Corrections.pkl", 'wb') as file:
+    #    pickle.dump((i, data), file)"""
--- NDPT_energy2.py
+++ NDPT_energy2.py
@@ -1,6 +1,5 @@
-from decimal import InvalidOperation
 import numpy as np
-import math
+from collections import defaultdict
 import pickle
 
 # rotate list ls to the right by n (negative to go left)
@@ -28,7 +27,7 @@ def list_cyclic_equal(l1, l2):
 def list_set_equal(l1, l2):
     return np.all(np.array([x in l2 for x in l1])) and np.all(np.array(x in l1 for x in l2))
 
-# Return all first/last-term-canceling partitions for correction n
+# Return all non first/last-term-canceling partitions for correction n
 def smart_partition(n):
     # returns all *lists* n non-negative numbers that sum to m
     def partition(n,m):
@@ -53,20 +52,41 @@ def smart_partition(n):
                 res.append([i] + ls + [j])
     return res
 
+class PartitionGenerator:
+    partitions = dict()
+    
+    def partition(self, n, m):
+        if (n,m) in PartitionGenerator.partitions.keys():
+            return PartitionGenerator.partitions[(n,m)]
+        res = []
+        if m == 0:
+            return [[0 for _ in range(n)]]
+        if n == 1:
+            return [[m]]
+        for i in range(m+1):
+            for ls in self.partition(n-1, m-i):
+                res += [[i] + ls]
+        PartitionGenerator.partitions[(n,m)] = res
+        return res
+    
+    def noncanceling_partitions(self, n):
+        res = []
+        # zero on each side
+        for ls in self.partition(n-1, n-1):
+            yield [0] + ls + [0]
+        # nonzero on each side
+        for i in range(1, n):
+            for j in range(1, n-i):
+                for ls in self.partition(n-1, n-1-i-j):
+                    yield [i] + ls + [j]
+        return res
+
 class MultiSet:
     def __init__(self) -> None:
-        self.items = []
+        self.items = defaultdict(int)
 
     def add(self, new_item, count, auto_clear=True):
-        need_to_append = True
-        for i in range(len(self.items)):
-            current_item, current_count = self.items[i]
-            if current_item == new_item:
-                self.items[i] = (current_item, current_count + count)
-                need_to_append = False
-                break
-        if need_to_append:
-            self.items.append(tuple((new_item, count)))
+        self.items[new_item] += count
         if auto_clear:
             self.clear_zero_count_items()
 
@@ -76,10 +96,17 @@ class MultiSet:
         self.clear_zero_count_items()
 
     def clear_zero_count_items(self):
-        self.items = [i for i in self.items if i[1] != 0]
+        new_items = defaultdict(int)
+        for k,v in self.items.items():
+            if v != 0:
+                new_items[k] = v
+        self.items = new_items
 
     def __eq__(self, other):
-        return list_set_equal(self.items, other.items)
+        return self.items == other.items
+    
+    def __hash__(self) -> int:
+        return hash(set(self.items.items()))
 
     def __str__(self):
         return " ".join(["{}^{:d}".format(i[0], i[1]) if i[1] != 1 else "{}".format(i[0]) for i in self.items])
@@ -94,6 +121,9 @@ class SigmaTerm:
     def __eq__(self, other: object) -> bool:
         return len(self.indices) == len(other.indices) and np.all(np.array(self.indices) == np.array(other.indices))
     
+    def __hash__(self):
+        return hash(tuple(self.indices))
+
     def __str__(self):
         return "Sigma({})".format(self.indices)
 
@@ -153,6 +183,9 @@ class PerturbativeTerm:
 
     def __eq__(self, other):
         return self.v_exp == other.v_exp and self.sigmas == other.sigmas
+    
+    def __hash__(self):
+        return hash((self.v_exp, self.sigmas))
 
     def __str__(self) -> str:
         return "(V_00 ^ {:d}) ".format(self.v_exp) + str(self.sigmas) if self.v_exp > 0 else ""
@@ -171,10 +204,8 @@ class EnergyCorrection:
     def calc(self):
         # Generate perms and combine first/last 
         p_terms = MultiSet()
-        foo = smart_partition(self.n)
-        bar = "eyyyylmao"
-        print(len(foo))
-        for part in foo:
+        partGen = PartitionGenerator()
+        for part in partGen.noncanceling_partitions(self.n):
             p_terms.add(PerturbativeTerm.FromPartition(part), (-1)**(part.count(0)), auto_clear=False)
         p_terms.clear_zero_count_items()
 
@@ -193,10 +224,14 @@ class EnergyCorrection:
     def to_list(self):
         return [(v_exp, [([(sigma.indices, exp) for sigma,exp in sigmas.sigmas.items], coeff) for sigmas,coeff in pterm]) for v_exp, pterm in self.SigmasByV00.items()]
     
-for i in range(2,13):
+correction = EnergyCorrection(4)
+correction.calc()
+correction.print_pterms_by_v00()
+
+"""for i in range(2,13):
     correction = EnergyCorrection(i)
     correction.calc()
     data = correction.to_list()
     print("Terms in correction {}: {}".format(i, len(correction.p_terms.items)))
     #with open("Coded_Corrections.pkl", 'wb') as file:
-    #    pickle.dump((i, data), file)
+    #    pickle.dump((i, data), file)"""
